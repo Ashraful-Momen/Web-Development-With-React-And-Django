b17d691a848da74365059259e17e481586e2a9ee
Md.Ashraful Momen <34467735+Ashraful-Momen@users.noreply.github.com>
2023-05-18 19:30:37 +0600

    variable and operator

---
+++ JavaScript/2. Variable and Constent/1. Variable Part 1-3/js/script.js
@@ -0,0 +1,23 @@
+// var a = 20;
+
+// let b = 30;
+
+// var c = a;
+
+// console.log(c);
+
+// a = 44;
+
+// console.log(c);// still c = 30 cz , c value not change at all;
+
+const pi = 3.1416; //constent have to initial value either when constent declear!!! either getting error.
+
+
+// var name-First = 30;
+// console.log(name$123);
+
+// let a;
+// let b= a;
+// a =90; 
+
+// console.log(b); // here b value will be undefined;
---
+++ JavaScript/3. Operator/1. Operator/js/script.js
@@ -0,0 +1,24 @@
+// arithmatic operator: +, - , *, /, % , ** , ++ , -- : 
+
+a = 5; 
+b = 6;
+
+
+console.log(a+b); //11
+console.log(a-b); //-1
+console.log(a*b); //30
+console.log(a/b);// 0.8333333333333334
+console.log(a**b); //15625
+console.log(a++); //5
+console.log(++a +" pre a increment"); //7 pre a increment
+console.log(b++); //6
+console.log(b++); //7
+console.log(b++); //8
+console.log(++b +" this is b pre increment"); //10 this is b pre increment
+
+
+// unani operator : single operation
+
+// binary operator: double operation
+
+// trinary operator: 3 operation
