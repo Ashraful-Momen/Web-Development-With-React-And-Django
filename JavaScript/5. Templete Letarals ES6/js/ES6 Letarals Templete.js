7cd03e1b092514bbdd02fa29f43a3326a86d0391
Md.Ashraful Momen <34467735+Ashraful-Momen@users.noreply.github.com>
2023-05-20 15:08:30 +0600

    Template Laterals ES6
    new line print , math expression.

---
+++ JavaScript/5. Templete Letarals ES6/js/ES6 Letarals Templete.js
@@ -0,0 +1,15 @@
+var a  = `How are you? 
+I hope you are quite well by the grace of Allah`;
+
+console.log(a);
+
+let b = 5;
+let c = 6 ;
+
+console.log(`The sum of ${b} + ${c} = ${b+c}`);
+
+var name = "Ashraful ";
+var age  = 26;
+var city = "Dhaka";
+console.log(`My name is ${name} and age is ${age}
+and I live in ${city}`);
