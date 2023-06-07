b78037dba75587c6d51357de235efbff259481f8
Md.Ashraful Momen <34467735+Ashraful-Momen@users.noreply.github.com>
2023-06-07 15:26:48 +0600

    validation check - postalCode,phone,email
    validation check - postalCode,phone,email

---
+++ JavaScript/12. Regular Expression/5. Validation-postalCode,phone&mail/reg-validation.js
@@ -0,0 +1,20 @@
+// Examples
+let re;
+let str;
+
+// Postal Code
+// 4700, 4000
+re = /^[0-9]{4}$/;
+str = "4000";
+
+// Phone Number
+// 01717888888 +8801717888888 8801717888888
+re = /^(+)?(88)?01[0-9]{9}$/;
+str = "+8801717888888";
+
+// Email Address
+// bohubrihi8.learn@edu.com.bd
+re = /^([a-zA-Z0-9].?)+[^.]@([a-zA-Z0-9].?)+[^.]$/ ;
+str = "bohubrihi.learn@edu.com.bd" ;
+
+console.log(re.test(str)); 
