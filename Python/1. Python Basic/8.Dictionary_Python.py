e2895180af1de56e89950498124b3490156cbb45
Md.Ashraful Momen <34467735+Ashraful-Momen@users.noreply.github.com>
2023-07-27 14:14:58 +0600

    Dictionary
    Dictionary

---
+++ Python/1. Python Basic/8.Dictionary_Python.py
@@ -0,0 +1,43 @@
+# info = {'name':'Karan', 'age':19, 'eligible':True}
+# print(info) 
+# print(info.keys())
+# print(info.values())
+
+# for key in info.keys():
+#   print(f"The value corresponding to the key {key} is {info[key]}")
+
+# print(info.items()) #Output=> dict_items([('name', 'Karan'), ('age', 19), ('eligible', True)])
+
+# for key, value in info.items():
+#   print(f"The value corresponding to the key {key} is {value}") 
+
+# ------------------------------------------Dictionary Method: ------------------------------------------
+
+# info = {'name':'Karan', 'age':19, 'eligible':True}
+# print(info)
+
+# info.update({'age':20})
+# info.update({'DOB':2001})
+# print(info)
+
+# info = {'name':'Karan', 'age':19, 'eligible':True}
+# info.clear()
+# print(info)
+
+# info = {'name':'Karan', 'age':19, 'eligible':True}
+# info.pop('eligible')
+# print(info)
+
+# info = {'name':'Karan', 'age':19, 'eligible':True, 'DOB':2003}
+# info.popitem()
+# print(info)
+
+# info = {'name':'Karan', 'age':19, 'eligible':True, 'DOB':2003}
+# del info['age']
+# print(info)
+
+
+# info = {'name':'Karan', 'age':19, 'eligible':True}
+# # get()	Returns the value of the specified key.
+
+# print(info.get('name'))
