66d31d3e84079e058e713d2fdf52b1737a945b28
Md.Ashraful Momen <34467735+Ashraful-Momen@users.noreply.github.com>
2023-09-08 19:43:18 +0600

    Update domSelector.js

--- JavaScript/10. DOM- Document Object Model/2. DOM -Selector (SIngle -Element)/js/domSelector.js
+++ JavaScript/10. DOM- Document Object Model/2. DOM -Selector (SIngle -Element)/js/domSelector.js
@@ -6,8 +6,10 @@
 
 // let val = document.getElementById('title-id');
 let val = document.getElementById('title-id').id; // return the id name : title-id
-val = document.getElementById('title-id').classList;
-val = document.getElementById('title-id').className;
+
+val = document.getElementById('title-id').classList; // return total class List  as array
+val = document.getElementById('title-id').className; //return class names...
+
 
 //changing style by adding css: 
 
@@ -33,9 +35,28 @@ val = document.getElementById('title-id').innerHTML="<i>Content make as italic <
 document.querySelector('#title-id').style.background= "green";
 document.querySelector('.root1').style.color= "black";
 
-val = document.querySelector('ol>li:nth-child(2n)').style.background="red";
-val = document.querySelector('ol>li:first-child').style.background="green";
+val = document.querySelector('ol>li:nth-child(2n)').style.background="red"; //work for an single element
+val = document.querySelectorAll('ol>li:nth-child(odd)').style.background="green"; // work for multiple element
+
 val = document.querySelector('ol>li:first-child').innerText="JavaScript";
  
 
-console.log(val);
+// console.log(val);
+
+
+// ----------------------------------------chatgpt---------------------------------------
+// want use directly as => document.querySelectorAll('ol>li:nth-child(odd)').style.background = 'green'; but getting error 
+
+// ChatGPT
+// The issue you're encountering is that document.querySelectorAll('ol>li:nth-child(odd)') returns a NodeList, which is similar to an array. You can't apply styles directly to a NodeList like you would to a single element.
+
+// If you want to apply a style to each odd <li> element, you'll need to iterate through the list and apply the style to each element individually. Here's how you can do it:
+
+// javascript
+// Copy code
+// document.querySelectorAll('ol>li:nth-child(odd)').forEach(function(li) {
+//     li.style.background = 'green';
+// });
+// This code selects all odd <li> elements and then iterates over each one, setting its background color to green.
+
+// Remember, this code should be executed in a script block after the elements have loaded on your page. If you're running it in the browser console, make sure you're on the correct page with the <ol> element before executing the command.
