67cf2c2be2f2a75b919ed7f2e1a4eaffad04251d
Md.Ashraful Momen <34467735+Ashraful-Momen@users.noreply.github.com>
2024-06-16 15:46:07 +0630

    Spread&Rest.js
    1.Spread : split array element into single element. 
    
    2. Rest : merge multiple single element into 1 array elements.

--- React/3. ES6 Refresher/4. Spread & Rest/Spread&Rest.js
+++ React/3. ES6 Refresher/4. Spread & Rest/Spread&Rest.js
@@ -2,7 +2,7 @@ let arr = [1,2,3,4];
 
 // console.log(arr);
 
-// console.log(...arr); // spread : split of an array element.
+// console.log(...arr); //*** spread : split of an array element into single element without array.
 
 let a=[1,2,3,4] , b = [5,6,7,8]
 
@@ -35,6 +35,8 @@ d = { ...person, nation : "Bangladesh"}; //spread : split element of object.
 
 // console.log(num(1,2,3,4)); // number varibale of the function can accept just 1 perameter.
 
+
+//*** rest operator merge multiple element into 1 array element ... ex: basically use to pass multiple value as an array with rest operator.
 
 let num = (...number) =>{  //Rest (function of paremeter receive): receive parameter as like python ***key.
     console.log(number); 
