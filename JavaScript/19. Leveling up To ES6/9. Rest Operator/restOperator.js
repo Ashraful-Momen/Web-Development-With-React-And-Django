72f6dad88a736054a0e859fe6fd5b3e98325e12f
Md.Ashraful Momen <34467735+Ashraful-Momen@users.noreply.github.com>
2025-08-24 16:42:26 +0600

    Update restOperator.js

--- JavaScript/19. Leveling up To ES6/9. Rest Operator/restOperator.js
+++ JavaScript/19. Leveling up To ES6/9. Rest Operator/restOperator.js
@@ -1,13 +1,19 @@
-// Rest Operator ... left side rule....
-// used : when making destructor , [destructor , used 'rest...'operator] = array/obj
+// Rest Operator ... (left side rule)
+// Used: when making destructuring patterns, [destructure, use 'rest...' operator] = array/obj
+// The rest operator collects the remaining elements into a new array/object
 
+// ARRAY EXAMPLE
 let fruits = ['apple', 'grape', 'mango', 'jackfruit'];
 let [first, second, ...third] = fruits
+// first = 'apple' (first element)
+// second = 'grape' (second element) 
+// ...third collects the REST of the elements = ['mango', 'jackfruit']
 
-// console.log(first);
-// console.log(second);
-// console.log(third);
+// console.log(first);    // Output: apple
+// console.log(second);   // Output: grape  
+// console.log(third);    // Output: ['mango', 'jackfruit']
 
+// OBJECT EXAMPLE
 let person = {
     fname: "Simanta",
     lname: "Paul",
@@ -15,16 +21,28 @@ let person = {
 }
 
 let {fname, ...lname} = person
+// fname = "Simanta" (extracts the fname property)
+// ...lname collects the REST of the properties = {lname: "Paul", dob: "8-26-1995"}
 
-// console.log(fname);
-// console.log(lname);
+// console.log(fname);    // Output: Simanta
+// console.log(lname);    // Output: {lname: "Paul", dob: "8-26-1995"}
 
+// FUNCTION PARAMETER EXAMPLE (REST operator)
 let moreNum = [78,1,2,5,6];
 
-let test =(name, ...numbers) => { // Rest : when function parameter
-    console.log(name);
-    console.log(numbers);
+let test = (name, ...numbers) => { // Rest: when used in function parameters
+    console.log(name);     // First argument
+    console.log(numbers);  // All remaining arguments collected into an array
 }
 
-test("Simanta", 67,3,3);
-test("Simanta", ...moreNum); // Spread : when call function
+test("Simanta", 67, 3, 3);
+// Output: 
+// Simanta
+// [67, 3, 3]
+
+test("Simanta", ...moreNum); // Spread: when calling function (right side)
+// The ...moreNum SPREADS the array into individual arguments
+// Equivalent to: test("Simanta", 78, 1, 2, 5, 6)
+// Output:
+// Simanta  
+// [78, 1, 2, 5, 6]
